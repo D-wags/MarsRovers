e5769c8acd9a5640434629604f987ec640b763eb
Drew Wagner <drew_wagner@utexas.edu>
2017-06-08 13:58:47 -0500

    Updated nav-bar and README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MarsRovers
+
+Displays images from Mars Rovers: Spirit, Opportunity & Curiosity, asteroid data and NASA Image of the day queried from NASA API.
+
+
--- assets/js/code.js
+++ assets/js/code.js
@@ -1,62 +1,131 @@
-$(document).on("ready",function(){
-	var rovers = ["curiosity","opportunity","spirit"];
+$(document).on("ready", function() {
+	var rovers = ["Curiosity", "Opportunity", "Spirit"];
 
 	var cameras = {
-		curiosity:[
-			{id: "FHAZ", name: "Front Hazard Avoidance Camera"},
-			{id: "RHAZ", name: "Rear Hazard Avoidance Camera"},
-			{id: "MAST", name: "Mast Camera"},
-			{id: "CHEMCAM", name: "Chemistry and Camera Complex"},
-			{id: "MAHLI", name: "Mars Hand Lens Imager"},
-			{id: "MARDI", name: "Mars Descent Imager"},
-			{id: "NAVCAM", name: "Navigation Camera"}
+		Curiosity: [
+			{ id: "FHAZ", name: "Front Hazard Avoidance Camera" },
+			{ id: "RHAZ", name: "Rear Hazard Avoidance Camera" },
+			{ id: "MAST", name: "Mast Camera" },
+			{ id: "CHEMCAM", name: "Chemistry and Camera Complex" },
+			{ id: "MAHLI", name: "Mars Hand Lens Imager" },
+			{ id: "MARDI", name: "Mars Descent Imager" },
+			{ id: "NAVCAM", name: "Navigation Camera" }
 		],
-		opportunity:[
-			{id: "FHAZ", name: "Front Hazard Avoidance Camera"},
-			{id: "RHAZ", name: "Rear Hazard Avoidance Camera"},
-			{id: "NAVCAM", name: "Navigation Camera"},
-			{id: "PANCAM", name: "Panoramic Camera"},
-			{id: "MINITES", name: "Miniature Thermal Emission Spectrometer (Mini-TES)"}
+		Opportunity: [
+			{ id: "FHAZ", name: "Front Hazard Avoidance Camera" },
+			{ id: "RHAZ", name: "Rear Hazard Avoidance Camera" },
+			{ id: "NAVCAM", name: "Navigation Camera" },
+			{ id: "PANCAM", name: "Panoramic Camera" },
+			{ id: "MINITES", name: "Miniature Thermal Emission Spectrometer (Mini-TES)" }
 		],
-		spirit:[
-			{id: "FHAZ", name: "Front Hazard Avoidance Camera"},
-			{id: "RHAZ", name: "Rear Hazard Avoidance Camera"},
-			{id: "NAVCAM", name: "Navigation Camera"},
-			{id: "PANCAM", name: "Panoramic Camera"},
-			{id: "MINITES", name: "Miniature Thermal Emission Spectrometer (Mini-TES)"}
+		Spirit: [
+			{ id: "FHAZ", name: "Front Hazard Avoidance Camera" },
+			{ id: "RHAZ", name: "Rear Hazard Avoidance Camera" },
+			{ id: "NAVCAM", name: "Navigation Camera" },
+			{ id: "PANCAM", name: "Panoramic Camera" },
+			{ id: "MINITES", name: "Miniature Thermal Emission Spectrometer (Mini-TES)" }
 		]
+	};
+	var roverDates = {
+		Curiosity : {
+			minDate: {
+				currentDate:false,
+				year:2012,
+				month:07,
+				day:06
+			},
+			maxDate:{
+				currentDate:true
+			}
+		},
+    	Opportunity : {
+    		minDate: {
+    			currentDate:false,
+    			year:2004,
+    			month:00,
+    			day:25
+    		},
+    		maxDate:{
+    			currentDate:true
+    		}
+    	},
+    	Spirit : {
+    		minDate: {
+    			currentDate:false,
+    			year:2004,
+    			month:00,
+    			day:04
+    		},
+    		maxDate:{
+    			currentDate:false,
+    			year:2010,
+    			month:02,
+    			day:22
+    		}
+    	}
 	};
 
 	var roverHtmlSelector = "#roverSelect";
 	var cameraHtmlSelector = "#cameraSelect";
+	var radioDateHtmlSelector = ".radioDate";
 	var nasaApiKey = "qVFWydcClpA2utQfaZBW0s0R70S0XQvDyh59Y2Jh";
+	var earthDateSelected = true;
 
-	rovers.forEach(function(r){
-		$(roverHtmlSelector).append($("<option value='"+r+"'>"+r+"</option>"));
+	rovers.forEach(function(r) {
+		$(roverHtmlSelector).append($("<option value='" + r + "'>" + r + "</option>"));
 	});
 	//Select a rover
-	rover = rovers[0];//default - first rover
-	updateCameras(cameraHtmlSelector,rover,cameras);
+	rover = rovers[0]; //default - first rover
+	updateCameras(cameraHtmlSelector, rover, cameras);
 
-	$(roverHtmlSelector).on("change",function(ev)
-	{
+	$(roverHtmlSelector).on("change", function(ev) {
 		//Setting currently changed option value to roverId variable
 		var roverId = $(this).find('option:selected').val();
-		updateCameras(cameraHtmlSelector,roverId,cameras);
+		updateCameras(cameraHtmlSelector, roverId, cameras);
+		var minDate = roverDates[roverId]["minDate"];
+		var maxDate = roverDates[roverId]["maxDate"];
+		updateCalendarDates(inputDate, minDate, maxDate)
 	});
 
-	$("#send").on("click",function(ev){
+	var earthDateHtmlSelector = "#earthDate";
+	var marsDateHtmlSelector = "#marsDate";
+	var inputDate = "#inputDate";
+	var inputSol = "#inputSol";
+
+	$(radioDateHtmlSelector).on("change", function(ev) {
+		//Disable an input depending on the date selected
+		var radioId = "#"+$(this).attr("id");
+
+		if(radioId === earthDateHtmlSelector)
+		{
+			earthDateSelected = true;
+			$(inputDate).prop("disabled",false);
+			$(inputSol).prop("disabled",true);
+		}
+		else
+		{
+			earthDateSelected = false;
+			$(inputDate).prop("disabled",true);
+			$(inputSol).prop("disabled",false);
+		}
+	});
+
+	updateCalendarDates(inputDate,roverDates[rover]["minDate"],roverDates[rover]["maxDate"]);
+
+	$("#send").on("click", function(ev) {
 		ev.preventDefault();
 		var roverId = $(roverHtmlSelector).find('option:selected').val();
 		var cameraId = $(cameraHtmlSelector).find('option:selected').val();
-		var nasaUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverId+"/photos";
+		var nasaUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverId + "/photos";
 		// var nasaUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
-		var dataObj = {api_key:nasaApiKey};
+		var dataObj = { api_key: nasaApiKey };
 
 		//MODIFY HERE!!!!
-		var earthDateSelected = false;
-		var earthDate = ""
-		var sol = 1000;
+		// we need to put in input validation statements here!!!!
+		// Curiosity landed 080520112
+		// Oppurtunity landed 06062005 - present
+		// Spirit landed 
+
 		//END MODIFY
 
 		if( cameraId.toLowerCase() !== "all" )
@@ -66,11 +135,15 @@ $(document).on("ready",function(){
 
 		if( earthDateSelected )
 		{
-			dataObj["earth_date"] = earthDate;
+			var selectedDate = $(inputDate).val().trim();
+			//ADD A VALIDATION HERE!!!
+			dataObj["earth_date"] = selectedDate;
 		}
 		else
 		{
-			dataObj["sol"] = parseInt(sol);
+			var selectedDate = $(inputSol).val().trim();            
+			//ADD A VALIDATION HERE!!!
+			dataObj["sol"] = parseInt(selectedDate);
 		}
 
 		//AJAX Call
@@ -78,36 +151,50 @@ $(document).on("ready",function(){
 			url: nasaUrl,
 			method: "GET",
 			data: dataObj
-		}).done(function(res){
+		}).done(function(res) {
 			//MODIFY HERE TOO!!
-			console.log(res);
 			$("#pics").empty();
 			var roverPics = res.photos;
-			// console.log(roverPics.length);
-
-			for (i =0; i < 5; i++) {
-				// console.log(roverPics[i].img_src);
+			for (i = 0; i < roverPics.length; i++) {
 				var roverPic = $("<img>");
 				roverPic.attr('src', roverPics[i].img_src);
 				$("#pics").append(roverPic);
 			}
-
-
 			//END MODIFY
-		}).fail(function(err){
+		}).fail(function(err) {
 			console.log(err);
-		});	
-
-
+		});
 	});
 
+	function updateCalendarDates(calendarHtmlSelector, minDate, maxDate) {
+		var mindt=new Date();
+		var maxdt=new Date();
+		
+		if(!minDate.currentDate)
+		{
+			mindt=new Date(minDate.year,minDate.month,minDate.day);
+		}
+
+		if(!maxDate.currentDate)
+		{
+			maxdt=new Date(maxDate.year,maxDate.month,maxDate.day);
+		}
+
+		$(calendarHtmlSelector).datepicker('destroy');
+
+
+		$(calendarHtmlSelector).datepicker({
+			dateFormat: 'yy-mm-dd',
+			minDate: mindt,
+			maxDate: maxdt
+		});
+	}
 	/* Function Update Cameras */
 	//--------------------------------
 	// cameraHtmlSelector: ID (HTML) of the "<select>" tag of the cameras
 	// roverId: ID (JS) of the selected rover
 	// cams: array of rover objects (with their cameras)
-	function updateCameras(cameraHtmlSelector,roverId,cams)
-	{
+	function updateCameras(cameraHtmlSelector, roverId, cams) {
 		//clean html element
 		$(cameraHtmlSelector).html("");
 		//get an array of cameras of the selected rover
@@ -115,10 +202,11 @@ $(document).on("ready",function(){
 		//append an option for all the cameras 
 		$(cameraHtmlSelector).append($("<option value='all'>All</option>"));
 		//for each rover
-		arrCameras.forEach(function(c){
+		arrCameras.forEach(function(c) {
 			//append an option to the select with an available cameras
-			$(cameraHtmlSelector).append($("<option value='"+c.id+"'>"+c.name+"</option>"));
+			$(cameraHtmlSelector).append($("<option value='" + c.id + "'>" + c.name + "</option>"));
 		});
+		$("#imageDiv").html("<img id='cur' src='assets/images/"+roverId+".jpg' height='150' width='150'>");
 	}
 	/* End Update Cameras */
 });
